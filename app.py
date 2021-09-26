e5e9c86dc4c9ad2ce20a0abe071a2d1fe9043280
ccecil-beast <91372302+ccecil-beast@users.noreply.github.com>
2021-09-26 16:38:36 +0530

    Create app.py
    In this file I have added some comments to source code explaining about our mission.

---
+++ app.py
@@ -0,0 +1,9 @@
+# this is a new change
+print("hi")
+'''
+All of these changes are beneficial to creating
+this app that I think will affect the universe
+in a way which is unrecedented
+'''
+
+print("The End")
